e377fe28b8d4355962de9a62b81ef054e3f46068
Oliver Scott <olivercscott@gmail.com>
2024-04-16 19:43:18 -0400

    Implement internet access for new apps setting
    Change-Id: Ie327d6025ca502e9a10bd402743e10caed6017fc

---
+++ app/src/main/java/org/calyxos/datura/service/DaturaService.kt
@@ -0,0 +1,88 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.service
+
+import android.Manifest
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.net.NetworkPolicyManager
+import android.os.IBinder
+import android.os.Process
+import android.os.UserHandle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import org.calyxos.datura.models.MinimalApp
+import org.calyxos.datura.utils.CommonUtils
+import org.calyxos.datura.utils.CommonUtils.PREFERENCE_DEFAULT_INTERNET
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class DaturaService : Service() {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private val TAG = DaturaService::class.java.simpleName
+
+    private val packageReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (context != null && intent?.action == Intent.ACTION_PACKAGE_ADDED) {
+                val app = getMinimalApp(intent.data!!.encodedSchemeSpecificPart)
+                val isUpdate = intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)
+
+                if (!isUpdate && app.requestsInternetPermission) {
+                    if (!sharedPreferences.getBoolean(PREFERENCE_DEFAULT_INTERNET, true)) {
+                        Log.i(TAG, "Auto-denying internet access for ${app.uid}")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        registerReceiver(
+            packageReceiver,
+            IntentFilter().apply {
+                addAction(Intent.ACTION_PACKAGE_ADDED)
+                addDataScheme("package")
+            }
+        )
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(packageReceiver)
+        super.onDestroy()
+    }
+
+    private fun getMinimalApp(packageName: String): MinimalApp {
+        val packageInfo = packageManager.getPackageInfoAsUser(
+            packageName,
+            PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong()),
+            UserHandle.getUserId(Process.myUid())
+        )
+        val requestsInternetPerm =
+            packageInfo.requestedPermissions?.contains(Manifest.permission.INTERNET) ?: false
+
+        return MinimalApp(
+            name = packageInfo.applicationInfo.loadLabel(packageManager).toString(),
+            packageName = packageInfo.packageName,
+            icon = CommonUtils.getIconForPackage(packageManager, packageInfo),
+            uid = packageInfo.applicationInfo.uid,
+            requestsInternetPermission = requestsInternetPerm
+        )
+    }
+}
