e36db94cfecdc51b4a13e701eec9edad6f11fd4b
Oliver Scott <olivercscott@gmail.com>
2024-04-18 20:06:21 -0400

    Copy Restrict cleartext network traffic setting
    Change-Id: I87406a965117230008f261f860b5f0bc63e0fb28

--- app/src/main/java/org/calyxos/datura/settings/SettingsFragment.kt
+++ app/src/main/java/org/calyxos/datura/settings/SettingsFragment.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import androidx.preference.Preference
+import android.os.StrictMode
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,8 @@ import org.calyxos.datura.R
 import org.calyxos.datura.service.DaturaService
 import org.calyxos.datura.utils.CommonUtils.PREFERENCE_DEFAULT_INTERNET
 import org.calyxos.datura.utils.CommonUtils.PREFERENCE_NOTIFICATIONS
+
+import lineageos.providers.LineageSettings
 
 @AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -41,6 +44,22 @@ class SettingsFragment : PreferenceFragmentCompat() {
                     startActivity(it)
                 }
                 true
+            }
+        }
+
+        findPreference<SwitchPreferenceCompat>("cleartext_network_policy")?.let {
+            it.isChecked = LineageSettings.Global.getInt(
+                context?.contentResolver,
+                LineageSettings.Global.CLEARTEXT_NETWORK_POLICY,
+                StrictMode.NETWORK_POLICY_INVALID
+            ) == StrictMode.NETWORK_POLICY_REJECT
+            it.setOnPreferenceChangeListener { _, newValue ->
+                LineageSettings.Global.putInt(
+                    context?.contentResolver,
+                    LineageSettings.Global.CLEARTEXT_NETWORK_POLICY,
+                    if (newValue as Boolean) StrictMode.NETWORK_POLICY_REJECT
+                    else StrictMode.NETWORK_POLICY_INVALID
+                )
             }
         }
     }
