63edd5760fa74fa522514fcae1c9ca6210c90539
Aayush Gupta <aayushgupta219@gmail.com>
2024-04-23 20:12:59 +0530

    Handle null-safety errors for AOSP QPR2
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ia314a9ca3b78b8e28657d2b0c34600e51e20417f

--- app/src/main/java/org/calyxos/datura/service/DaturaService.kt
+++ app/src/main/java/org/calyxos/datura/service/DaturaService.kt
@@ -43,7 +43,7 @@ class DaturaService : Service() {
 
         override fun onReceive(context: Context?, intent: Intent?) {
             if (context != null && intent?.action == Intent.ACTION_PACKAGE_ADDED) {
-                val app = getMinimalApp(intent.data!!.encodedSchemeSpecificPart)
+                val app = getMinimalApp(intent.data!!.encodedSchemeSpecificPart) ?: return
                 val isUpdate = intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)
 
                 if (!isUpdate && app.requestsInternetPermission) {
@@ -66,7 +66,7 @@ class DaturaService : Service() {
     override fun onCreate() {
         super.onCreate()
 
-        notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager = getSystemService(NotificationManager::class.java)!!
         notificationManager.createNotificationChannel(
             NotificationUtil.getAlertNotificationChannel(this)
         )
@@ -85,20 +85,26 @@ class DaturaService : Service() {
         super.onDestroy()
     }
 
-    private fun getMinimalApp(packageName: String): MinimalApp {
+    private fun getMinimalApp(packageName: String): MinimalApp? {
         val packageInfo = packageManager.getPackageInfoAsUser(
             packageName,
             PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong()),
             UserHandle.getUserId(Process.myUid())
         )
+
+        if (!Process.isApplicationUid(packageInfo.applicationInfo?.uid ?: Process.INVALID_UID)) {
+            Log.i(TAG, "$packageName is not an app")
+            return null
+        }
+
         val requestsInternetPerm =
             packageInfo.requestedPermissions?.contains(Manifest.permission.INTERNET) ?: false
 
         return MinimalApp(
-            name = packageInfo.applicationInfo.loadLabel(packageManager).toString(),
+            name = packageInfo.applicationInfo!!.loadLabel(packageManager).toString(),
             packageName = packageInfo.packageName,
             icon = CommonUtils.getIconForPackage(packageManager, packageInfo),
-            uid = packageInfo.applicationInfo.uid,
+            uid = packageInfo.applicationInfo!!.uid,
             requestsInternetPermission = requestsInternetPerm
         )
     }
